9812ecd76df93a319048f67553855ea1c23d9e8c
Johannes Mueller <johannes.mueller@byteleaf.de>
2017-12-17 15:00:40 +0100

    Log closer to clone

--- src/app/clone-performance/clone-details/clone-details.component.ts
+++ src/app/clone-performance/clone-details/clone-details.component.ts
@@ -36,19 +36,17 @@ export class CloneDetailsComponent implements OnInit {
     this.clones$ = this.activatedRoute.params
       .map((params: Params) => +params['count'])
       .filter((count: number) => count <= 10000)
-      .do((count: number) => this.pls.log(`Cloning ${count} elements...`))
       .switchMap(() => this.dataSampleService.loadWorldBankSample(),
         (count: number, data: any) => Object.assign({count: count, data: data}))
       .map((countAndData: CountAndData) =>
         this.cloneService.cloneMultipleTimes(countAndData.data, CloneTypes.DeepNative, countAndData.count))
-      .map((clones: any[][]) => clones.pop())
+      .map((clones: any[]) => clones.pop())
       .map((clones: any[]) => clones.map((worldBankDto: WorldBankDto) => Object.assign({
         approvalfy: worldBankDto.approvalfy,
         board_approval_month: worldBankDto.board_approval_month,
         lendprojectcost: worldBankDto.lendprojectcost,
         lendinginstr: worldBankDto.lendinginstr,
         country_namecode: worldBankDto.country_namecode
-      })))
-      .do(() => this.pls.log('Finished!'));
+      })));
   }
 }
--- src/app/core/services/clone.service.ts
+++ src/app/core/services/clone.service.ts
@@ -1,23 +1,33 @@
 import { Injectable } from '@angular/core';
 import * as _ from 'lodash';
 import { CloneTypes } from '../enums/clone-type';
+import { PerformanceLogService } from './performance-log.service';
 
 @Injectable()
 export class CloneService {
 
+  constructor(private pls: PerformanceLogService) {
+  }
+
   cloneMultipleTimes(object: any, cloneType: CloneTypes, amount = 1): any[] {
+    this.pls.log(`Cloning ${amount} elements...`);
+    let result = [];
     switch (cloneType) {
       case CloneTypes.DeepNative:
-        return this.createClones(object, this.deepCloneNative, amount);
+        result = this.createClones(object, this.deepCloneNative, amount);
+        break;
       case CloneTypes.DeepLodash:
-        return this.createClones(object, this.deepCloneLodash, amount);
+        result = this.createClones(object, this.deepCloneLodash, amount);
+        break;
       case CloneTypes.ShallowES6:
-        return this.createClones(object, this.shallowCloneES6, amount);
+        result = this.createClones(object, this.shallowCloneES6, amount);
+        break;
       case CloneTypes.ShallowLodash:
-        return this.createClones(object, this.shallowCloneLodash, amount);
-      default:
-        return [];
+        result = this.createClones(object, this.shallowCloneLodash, amount);
+        break;
     }
+    this.pls.log(`Finished ${cloneType} clone!`);
+    return result;
   }
 
   private createClones(object: any, cloneFunction: (object: any) => any, amount: number): any[] {
